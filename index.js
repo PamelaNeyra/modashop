65b441ae610f05e5f7a8bb727fb55421b0628fec
Jose1601 <jose.bustamante9@unmsm.edu.pe>
2019-09-27 13:37:36 -0500

    Deploying

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = require('./app');
 var port = process.env.PORT || 3977; // PUERTO QUE TENDRA NUESTRO SERVIDOR WEB  DEL BACKEND
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://junior:junior1@ds115753.mlab.com:15753/sistema-ventas', (err,res)=>{
+mongoose.connect('mongodb://admin:Prueba$1@clusterne-shard-00-00-np7qt.mongodb.net:27017,clusterne-shard-00-01-np7qt.mongodb.net:27017,clusterne-shard-00-02-np7qt.mongodb.net:27017/db_fisol?ssl=true&replicaSet=ClusterNE-shard-0&authSource=admin&retryWrites=true&w=majority', (err,res)=>{
 //mongoose.createConnection('mongodb://localhost:27017/evaluacionDB', (err,res)=>{
 
 //mongoose.connect('mongodb://localhost:27017/proyectoIhcDB', (err,res)=>{
